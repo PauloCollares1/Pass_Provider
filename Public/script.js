bdcf29e32b4461f3c639a8f4b301c160f4b723e9
Pauloc1Dev <paulocollares1@hotmail.com>
2022-03-22 19:40:16 -0300

    Estilizando CSS

--- Public/script.js
+++ Public/script.js
@@ -1,5 +1,4 @@
 
-
 // ---- Pegando as variaveis do meu body ---- //
 let nome_display = document.getElementById('nome_html');
 let tamanho_display = document.getElementById('tamanho_html');
@@ -10,6 +9,9 @@ let numeros_display = true;
 
 // ---- levando as variaveis para o body ---- //
 let imprime_valores_display = document.getElementById('imprime_valores');
+let imprime_botao = document.getElementById('botao');
+
+var esseaqui = false;
 
 // ---- funções ---- //
 function pega_valores(){
@@ -29,6 +31,7 @@ function pega_valores(){
     }
     fetch('/api',options)
     recebe_valores();
+    meubotao();
     // Tive que comentar as 2 linhas pois ambas estavam enviando nulo pra o objeto
     //nome_display = document.getElementById('nome_html').value = '';
     //tamanho_display = document.getElementById('tamanho_html').value = '';  
@@ -39,9 +42,28 @@ function recebe_valores(){
     fetch('/api')
         .then(response => response.json())
         .then(response => {
-                let add_html = (`<div class="teste"> <b>Nome:</b>`+` `+ response.nome +` `+`<b>Senha:</b>`+` `+ response.senha +`</div>`)
-                imprime_valores_display.insertAdjacentHTML('beforeend', add_html)
+
+                let add_html = (`<div class="teste"> <b>Nome:</b>`+` `+ response.nome +` `+
+                `<b>Senha:</b>`+` `+ response.senha +`</div>`)
+
+                imprime_valores_display.insertAdjacentHTML('afterbegin', add_html)
                 // dessa forma ele não aceita mais de um click: //imprime_valores_display.innerHTML = add_html
         })
+        
 }
+
+
+function meubotao(){
+
+    if(esseaqui === false){
+        esseaqui = (`<button class="gerar_senha" onclick="pega_valores()">Limpar</button>`)
+        imprime_botao.insertAdjacentHTML('beforeend', esseaqui)
+        console.log('eu entrei aqui')
+        return esseaqui = true;
+    }
+}
+
+
+
+
 
