0122733f291e6762d8c6b493a7844e95b28980d5
Pauloc1Dev <paulocollares1@hotmail.com>
2022-03-23 15:38:06 -0300

    adicionando checkbox opções

--- Public/script.js
+++ Public/script.js
@@ -2,10 +2,11 @@
 // ---- Pegando as variaveis do meu body ---- //
 let nome_display = document.getElementById('nome_html');
 let tamanho_display = document.getElementById('tamanho_html');
-let minuscula_display = true;
-let maiuscula_display = true;
-let simbolos_display = true;
-let numeros_display = true;
+let minuscula_display = document.getElementById('minuscula_html');
+let maiuscula_display = document.getElementById('maiuscula_html');
+let simbolos_display = document.getElementById('simbolos_html');
+let numeros_display = document.getElementById('numeros_html');
+
 
 // ---- levando as variaveis para o body ---- //
 let imprime_valores_display = document.getElementById('imprime_valores');
@@ -22,10 +23,10 @@ function pega_valores(){
     const formulario = { 
         nome_script : nome_display.value,
         tamanho_script : tamanho_display.value,
-        minuscula_script : minuscula_display,
-        maiuscula_script : maiuscula_display,
-        simbolos_script : simbolos_display,
-        numeros_script : numeros_display,
+        minuscula_script : minuscula,
+        maiuscula_script : maiuscula,
+        simbolos_script : simbolos,
+        numeros_script : numeros,
     }
     options = {
         method: "POST",
@@ -40,7 +41,7 @@ function pega_valores(){
     //tamanho_display = document.getElementById('tamanho_html').value = '';  
 }
 
-function recebe_valores(){
+function recebe_valores(){ 
 
     fetch('/api')
         .then(response => response.json())
@@ -65,15 +66,40 @@ function meubotao(){
         esseaqui = (`<button class="gerar_senha" onclick="Limpar()"><b>Limpar</b></button>`+
                     `<br><button class="gerar_senha" onclick="Limpar()"><b>Me manda no ZAP!</b></button>`)
         imprime_botao.insertAdjacentHTML('beforeend', esseaqui)
-        console.log('eu entrei aqui')
         return esseaqui = true;
     }
 }
 
+
+let minuscula = minuscula_display.addEventListener('change', (e) => {
+    minuscula = e.target.checked;
+    console.log("aaa: " + e.target.checked)
+})
+let maiuscula = maiuscula_display.addEventListener('change', (e) => {
+    maiuscula = e.target.checked;
+    console.log("AAA: " + e.target.checked)
+})
+let numeros = numeros_display.addEventListener('change', (e) => {
+    numeros = e.target.checked;
+    console.log("123: " + e.target.checked)
+})
+let simbolos = simbolos_display.addEventListener('change', (e) => {
+    simbolos = e.target.checked;
+    console.log("!@#: " + e.target.checked)
+})
+if(!minuscula && !maiuscula && !numeros && !simbolos){
+    minuscula = true;
+    maiuscula=true,
+    numeros=true,
+    simbolos = true;
+}
+
+
 darkmode.addEventListener('change', (e) => {
-    console.log(e.target.checked)
+
     document.body.classList.toggle("dark", e.target.checked);
 })
+
 
 
 
